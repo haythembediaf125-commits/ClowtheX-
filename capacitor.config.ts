d1b427801c378ea6f52a2722cb4966264e6890ba
haythembediaf125-commits <haythembediaf125@gmail.com>
2026-04-29 15:16:21 +0200

    Update appId and appName in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.haythemgroup.stylestockmanager",
-  appName: "Style Stock Manager",
+  appId: "com.haythemgroup.clowthex",
+  appName: "ClowtheX",
   webDir: "dist",
   bundledWebRuntime: false,
   android: {
