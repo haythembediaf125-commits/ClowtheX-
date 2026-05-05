f640a4a14a7fdbad9aa23fa4df910a418f297037
haythembediaf125-commits <haythembediaf125@gmail.com>
2026-05-05 16:40:37 +0200

    revert: restore capacitor.config.ts to original

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,7 @@ const config: CapacitorConfig = {
   appId: "com.haythemgroup.clowthex",
   appName: "ClowtheX",
   webDir: "dist",
+  bundledWebRuntime: false,
   android: {
     allowMixedContent: false,
   },
